f9df55a8038889c4ea682b060a7801597ad5284d
MirArfan <mir.arfan.iiuc9@gmail.com>
2024-02-27 15:23:39 +0600

    make a form for POST method using promise

--- app.js
+++ app.js
@@ -50,6 +50,79 @@ if (show_data_normally) {
 
 }
 
+
+/////........form section for POST method ......////
+const msg_btn = document.getElementById('msg-btn');
+
+
+let formVisible = false;
+
+msg_btn.addEventListener("click", function () {
+    if (!formVisible) {
+        const form_div = document.createElement("div");
+        form_div.style.display = "flex";
+        form_div.style.justifyContent = "center";
+        form_div.style.alignItems = "center";
+        form_div.style.maxWidth = "1440px";
+        // form_div.style.height = "40vh";
+        form_div.style.margin = "0 auto";
+        // form_div.style.width = "100vh";
+        form_div.style.padding = "20px";
+        form_div.style.borderRadius = "5px";
+
+        form_div.innerHTML = `
+    <form id="contact-form">
+    <label for="name">Name:</label><br>
+    <input type="text" id="name" name="name" required style="width: 400px; padding: 8px; margin-bottom: 10px; border: 1px solid #ccc; border-radius: 3px; "><br>
+    <label for="email">Email:</label><br>
+    <input type="email" id="email" name="email" required style="width: 400px; padding: 8px; margin-bottom: 10px; border: 1px solid #ccc; border-radius: 3px; box-sizing: border-box;"><br>
+    <label for="message">Message:</label><br>
+    <textarea id="message" name="message" placeholder="Optional" style="width: 400px; padding: 8px; margin-bottom: 10px; border: 1px solid #ccc; border-radius: 3px; box-sizing: border-box;"></textarea><br>
+    <button class="form-submit-btn" type="submit" style="width: 100px; padding: 10px; border: none; border-radius: 3px; background-color: #007bff; color: #fff; cursor: pointer;">Submit</button>
+</form>
+    `;
+
+        msg_btn.insertAdjacentElement('afterend', form_div);
+
+        const contact_form = document.getElementById('contact-form');
+        contact_form.addEventListener('submit', function (event) {
+            event.preventDefault();
+            const formData = new FormData(contact_form);
+            const formValues = Object.fromEntries(formData.entries());
+            console.log(formValues);
+            contact_form.reset();
+
+            fetch(apiEndPoint, {
+                method: 'POST',
+                body: JSON.stringify(formValues),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(response => {
+                if (response.ok) {
+                    console.log('Form submitted successfully');
+
+                } else {
+                    console.error('Form submission failed');
+
+                }
+            }).catch(error => {
+                console.error('Error submitting form:', error);
+            });
+        });
+        formVisible = true;
+    }
+    else {
+        const form_div = document.getElementById('contact-form');
+        if (form_div) {
+            form_div.remove();
+        }
+        formVisible = false;
+    }
+});
+
+
+
 // ............show data with Tabular formet...........//
 let show_data_tabular = true;
 
@@ -109,10 +182,10 @@ btn2.addEventListener('click', function () {
 
 // const fom = document.getElementById('form-btn');
 // fom.addEventListener('click', function(){
-    
+
 // })
 // form.addEventListener('submit', function () {
-   
+
 //     // let name = document.getElementById('name').value;
 //     // let body = document.getElementById('body').value;
 
@@ -121,7 +194,7 @@ btn2.addEventListener('click', function () {
 //     //     body: JSON.stringify({
 //     //         title: name,
 //     //         body: body,
-           
+
 //     //     }),
 //     //     headers: {
 //     //         'Content-type': 'application/json; charset=UTF-8',
